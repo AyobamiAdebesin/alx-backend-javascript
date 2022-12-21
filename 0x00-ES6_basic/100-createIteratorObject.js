e1d91f1baf141bcc387eec38ac0d061a51954bcb
AyobamiAdebesin <froshofficial@gmail.com>
2022-12-21 15:45:05 +0100

    Fixed typo in push keyword

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -2,7 +2,7 @@ export default function createIteratorObject(report) {
   const result = [];
   const getReportValues = report.allEmployees;
   for (const values of Object.values(getReportValues)) {
-    result.oush(...values);
+    result.push(...values);
   }
   return result;
 }
