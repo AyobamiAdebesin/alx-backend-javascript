75900e5a0f23e6e6881248aed4f5f8ecefcdf848
AyobamiAdebesin <froshofficial@gmail.com>
2022-12-21 13:13:58 +0100

    Removed extra -

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,7 +1,7 @@
 export default function appendToEachArrayValue(array, appendString) {
   const newArray = [];
   for (const val of array) {
-    newArray.push(`${appendString}-${val}`);
+    newArray.push(`${appendString}${val}`);
   }
   return newArray;
 }
