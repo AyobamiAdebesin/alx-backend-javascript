69c8fa1c8ae38776e7a9dfc23d7c15e07190c5be
AyobamiAdebesin <froshofficial@gmail.com>
2023-01-02 12:31:11 +0100

    Array for getting list of students

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,8 @@
+export default function getListStudents() {
+  const studentArray = new Array([]);
+  studentArray.push({ id: 1, firstName: 'Guillame', location: 'San Francisco' });
+  studentArray.push({ id: 2, firstName: 'James', location: 'Columbia' });
+  studentArray.push({ id: 5, firstName: 'Serena', location: 'San Francisco' });
+
+  return studentArray;
+}
