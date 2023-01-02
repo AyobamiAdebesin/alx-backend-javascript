6985a781b87c21fb74c7c50a96ac59230d078906
AyobamiAdebesin <froshofficial@gmail.com>
2023-01-02 14:04:21 +0100

    refactored with eslint

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,3 +1,3 @@
 export default function getStudentsByLocation(objArray, city) {
-  return (objArray.filter((element) => {return element.location === city}));
+  return (objArray.filter((element) => element.location === city));
 }
