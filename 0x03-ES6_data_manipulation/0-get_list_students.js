dec99260622ab7e04a0161fe19f2dca02956c9f0
AyobamiAdebesin <froshofficial@gmail.com>
2023-01-02 18:10:21 +0100

    Fixed typo in code

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,6 +1,6 @@
 export default function getListStudents() {
   const studentArray = [];
-  studentArray.push({ id: 1, firstName: 'Guillame', location: 'San Francisco' });
+  studentArray.push({ id: 1, firstName: 'Guillaume', location: 'San Francisco' });
   studentArray.push({ id: 2, firstName: 'James', location: 'Columbia' });
   studentArray.push({ id: 5, firstName: 'Serena', location: 'San Francisco' });
 
