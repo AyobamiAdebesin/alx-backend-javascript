85383ac68f8f0d958494d84e62fe217eae87c10d
AyobamiAdebesin <froshofficial@gmail.com>
2023-01-04 09:45:21 +0100

    Refacto: added check for an empty string input

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,8 @@
 export default function cleanSet(setObj, startString) {
   let outString = '';
+  if (startString === '') {
+    return '';
+  }
   for (const x of setObj) {
     if (x.startsWith(startString)) {
       outString += `${x.split(startString)[1]}-`;
