c5c512a23ccaa79ebf6364daeb18c358bb8157d9
AyobamiAdebesin <froshofficial@gmail.com>
2023-01-14 15:46:04 +0100

    Updated logic to give correct output

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,15 +1,30 @@
-export default function updateStudentGradeByCity(objArray, city, newGrades) {
-  const arr = objArray.filter((element) => element.location === city);
+export default function updateStudentGradeByCity(students, city, newGrades) {
+  if (!Array.isArray(students) || !Array.isArray(newGrades)) {
+    return [];
+  }
+  // Filter students by location
+  // Change/add grade for students in newGrades
+  // If a filtered student is not in new Grades => grade = N/A
 
-  const arr1 = arr.map((element) => {
-    for (const x of Object.keys(newGrades)) {
-      if (element.id === newGrades[x].studentId) {
-        const output = { grade: newGrades[x].grade };
-      } else {
-        const output = { grade: 'N/A' };
-      }
-    }
-    return output;
+  const cityStudents = students.filter((student) => student.location === city).map((student) => {
+    const item2 = newGrades.find((student2) => student.id === student2.studentId);
+    return { ...student, grade: item2 ? item2.grade : 'N/A' };
   });
-  return arr1;
+  return cityStudents;
 }
+
+// export default function updateStudentGradeByCity(objArray, city, newGrades) {
+//   const arr = objArray.filter((element) => element.location === city);
+
+//   const arr1 = arr.map((element) => {
+//     for (const x of Object.keys(newGrades)) {
+//       if (element.id === newGrades[x].studentId) {
+//         const output = { grade: newGrades[x].grade };
+//       } else {
+//         const output = { grade: 'N/A' };
+//       }
+//     }
+//     return output;
+//   });
+//   return arr1;
+// }
