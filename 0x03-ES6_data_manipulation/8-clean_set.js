472353b3ca2e59342e2ae8a05f0b4bd1116661c9
AyobamiAdebesin <froshofficial@gmail.com>
2023-01-14 17:53:37 +0100

    Lints code

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,6 +1,6 @@
 export default function cleanSet(setObj, startString) {
   let outString = '';
-  if (startString === undefined || startString.length === 0) {
+  if (startString === undefined || startString.length === 0 || typeof(startString) !== 'string') {
     return '';
   }
   for (const x of setObj) {
