ce46f2067fed63c4bf0de4f987b96700e46e6bf8
AyobamiAdebesin <froshofficial@gmail.com>
2023-01-15 10:06:17 +0100

    Created interface for a class and exposing the interface to the class

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,19 +1,47 @@
 interface Teacher {
-    readonly firstName : string,
-    readonly lastName : string,
-    fullTimeEmployee : boolean,
-    yearsOfExperience? : number,
-    location : string,
+    readonly firstName: string,
+    readonly lastName: string,
+    fullTimeEmployee: boolean,
+    yearsOfExperience?: number,
+    location: string,
     [attrName: string]: any,
 }
 
-interface Directors extends Teacher{
-    numberOfReports : number,
+// Extending the Teacher interface
+interface Directors extends Teacher {
+    numberOfReports: number,
 }
 
-interface printTeacherFunction{
-    (firstName: string, lastName: string) : string;
+// Creating interface for a function
+interface printTeacherFunction {
+    (firstName: string, lastName: string): string;
 }
-const printTeacher: printTeacherFunction = (firstName : string, lastName: string) : string => {
+// Function implementing the interface
+const printTeacher: printTeacherFunction = (firstName: string, lastName: string): string => {
     return `${firstName.charAt(0)}.${lastName}`;
 }
+
+// Implementing an interface for the StudentClass
+interface StudentClassInterface {
+    workOnHomeWork(): string,
+    displayName(): string,
+}
+
+// Creating a StudentClass that implements the interface
+class StudentClass implements StudentClassInterface {
+    firstName: string;
+    lastName: string;
+    constructor(firstName: string, lastName: string) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    workOnHomeWork(): string {
+        return "Currently working";
+    }
+
+    displayName(): string {
+        return this.firstName;
+    }
+}
+
