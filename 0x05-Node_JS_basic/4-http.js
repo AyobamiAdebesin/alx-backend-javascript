02fca71c726b01e6721cef91ba049602ae04b937
AyobamiAdebesin <froshofficial@gmail.com>
2023-03-06 13:15:58 +0100

    Spawning a simple web server in node

---
+++ 0x05-Node_JS_basic/4-http.js
@@ -0,0 +1,16 @@
+const http = require('http');
+
+/**
+ * Create a small HTTP server using the http module
+ */
+
+// Set the port and hostname
+const hostname = 'localhost';
+const port = 1245;
+
+const app = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello Holberton School!');
+});
+app.listen(port, hostname);
